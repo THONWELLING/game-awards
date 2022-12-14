20c498ebfaa7785d1c02c0a795a590c7d072fea6
THONWELLING <thonwellingdani13@gmail.com>
2022-12-13 22:54:12 -0300

    First Class

---
+++ src/main/java/com/thonwelling/gameawards/domain/model/Game.java
@@ -0,0 +1,57 @@
+package com.thonwelling.gameawards.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "GAMES")
+public class Game {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private String cover;
+  private long votes;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getCover() {
+    return cover;
+  }
+
+  public void setCover(String cover) {
+    this.cover = cover;
+  }
+
+  public long getVotes() {
+    return votes;
+  }
+
+  public void setVotes(long votes) {
+    this.votes = votes;
+  }
+}
