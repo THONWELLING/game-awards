05069abd2a50dd90d0ecbaa2db5b45e376030435
THONWELLING <thonwellingdani13@gmail.com>
2022-12-13 22:40:56 -0300

    Project Created

---
+++ src/main/java/com/thonwelling/gameawards/GameAwardsApplication.java
@@ -0,0 +1,15 @@
+package com.thonwelling.gameawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameAwardsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GameAwardsApplication.class, args);
+		System.out.println("The Project Is Rnning At The Port 8080 =====>");
+	}
+
+}
---
+++ src/test/java/com/thonwelling/gameawards/GameAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thonwelling.gameawards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
