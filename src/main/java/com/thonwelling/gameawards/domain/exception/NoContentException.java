032ad2b7f7a11556714fe3dac38175fbed6ff720
THONWELLING <thonwellingdani13@gmail.com>
2023-06-08 14:29:44 -0300

    Create a General Exception And Implementing primary CRUD

---
+++ src/main/java/com/thonwelling/gameawards/domain/exception/NoContentException.java
@@ -0,0 +1,9 @@
+package com.thonwelling.gameawards.domain.exception;
+
+import java.io.Serial;
+
+public class NoContentException extends RuntimeException{
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/thonwelling/gameawards/domain/services/impl/GameServiceImpl.java
@@ -0,0 +1,50 @@
+package com.thonwelling.gameawards.domain.services.impl;
+
+import com.thonwelling.gameawards.domain.model.Game;
+import com.thonwelling.gameawards.domain.repositories.GameRepository;
+import com.thonwelling.gameawards.domain.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class GameServiceImpl implements GameService {
+
+  @Autowired
+  private GameRepository gameRepository;
+
+  @Override
+  public List<Game> findAllGames() {
+    return gameRepository.findAll();
+  }
+
+  @Override
+  public Game findGameById(Long id) {
+    Optional<Game> game = gameRepository.findById(id);
+    return game.orElseThrow(() -> new RuntimeException("Could Not Find Game"));
+  }
+
+  @Override
+  public void addGame(Game game) {
+
+  }
+
+  @Override
+  public void updateGame(Long id, Game game) {
+    Game persistedGame = findGameById(id);
+    if (persistedGame.getId().equals(game.getId())) {
+      gameRepository.save(game);
+    } else {
+      throw new RuntimeException("The Ids To Update Are Differents");
+    }
+  }
+
+  @Override
+  public void deleteGame(Long id) {
+    Game persistedGame = findGameById(id);
+    gameRepository.delete(persistedGame);
+  }
+}
